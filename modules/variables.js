42f73b9e3580f962ed24da3838b9331ed958bc9b
jolak5 <kayodej4@gmail.com>
2023-02-21 10:14:12 +0100

    Break down the functions into modules

---
+++ modules/variables.js
@@ -0,0 +1,15 @@
+const awesomeBooksSection = document.getElementById('awesome-book');
+const inputFormSection = document.getElementById('input-forms');
+const showListButton = document.getElementById('show-list-buttons');
+const addNewButton = document.getElementById('add-new-button');
+const contactInfoSection = document.getElementById('contact-infos');
+const contactInfoButton = document.getElementById('contact-info-buttons');
+
+export {
+  showListButton,
+  contactInfoSection,
+  addNewButton,
+  contactInfoButton,
+  awesomeBooksSection,
+};
+export { inputFormSection };
